4933bcd4a8f21dba10fd854fdb0454e6072b0714
ASHUTOSH DWIVEDI <ashutosh.dwivedi@galgotiacollege.edu>
2020-07-15 20:54:37 +0530

    ADD: Notify.js to send FCMNotification

--- server/firebase/firebaseInit.js
+++ server/firebase/firebaseInit.js
@@ -2,10 +2,11 @@ const admin = require('firebase-admin');
 
 
 const serviceAccount = require('../firebase/firebase-admin-creds.json');
+require('dotenv').config()
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://tconsulta-7abcc.firebaseio.com'
+    databaseURL: process.env.FIREBASE_DB_URL
 });
 
 module.exports = {
